910c9291c92500eb80dba524aac0c2c92bf76370
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2018-09-09 23:16:05 -0400

    not working

--- LocalSettings.php
+++ LocalSettings.php
@@ -371,10 +371,6 @@ $wgAddGroups['steward'] = array( 'sysop', 'bureaucrat', 'steward', 'checkuser', 
 $wgRemoveGroups['steward'] = array( 'sysop', 'bureaucrat', 'steward', 'checkuser', 'oversight' );
 $wgAddGroups['bureaucrat'] = array( 'bot', 'sysop', 'bureaucrat' );
 $wgRemoveGroups['bureaucrat'] = array( 'bot', 'sysop', 'bureaucrat' );
-$wgGroupsRemoveFromSelf => array(
-	'sysop' => array( 'sysop' ),
-	'bureaucrat' => array( 'bureaucrat' ),
-);
 $wgRestrictionLevels = array( '', 'user', 'bureaucrat', 'sysop', 'autoconfirmed', 'steward' );
 $wgRestrictionTypes = array( 'create', 'edit', 'move', 'upload', 'delete', 'protect' );
 unset( $wgGroupPermissions['staff'] );
