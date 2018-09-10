19d326f3156739e65af38b3d7a545494c3352a9f
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2018-09-09 23:12:33 -0400

    fix remove from self settings

--- LocalSettings.php
+++ LocalSettings.php
@@ -371,7 +371,10 @@ $wgAddGroups['steward'] = array( 'sysop', 'bureaucrat', 'steward', 'checkuser', 
 $wgRemoveGroups['steward'] = array( 'sysop', 'bureaucrat', 'steward', 'checkuser', 'oversight' );
 $wgAddGroups['bureaucrat'] = array( 'bot', 'sysop', 'bureaucrat' );
 $wgRemoveGroups['bureaucrat'] = array( 'bot', 'sysop', 'bureaucrat' );
-$wgGroupsRemoveFromSelf = [ '*' => true ];
+$wgGroupsRemoveFromSelf => array(
+	'sysop' => array( 'sysop' ),
+	'bureaucrat' => array ( 'bureaucrat' ),
+);
 $wgRestrictionLevels = array( '', 'user', 'bureaucrat', 'sysop', 'autoconfirmed', 'steward' );
 $wgRestrictionTypes = array( 'create', 'edit', 'move', 'upload', 'delete', 'protect' );
 unset( $wgGroupPermissions['staff'] );
